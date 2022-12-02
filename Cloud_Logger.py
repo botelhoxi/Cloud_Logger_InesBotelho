aaa6487d954972cbad6aefd7270e0cbff6d5d6f0
botelhoxi <i.botelho@campus.fct.unl.pt>
2022-12-02 16:18:07 +0000

    a

--- Cloud_Logger.py
+++ Cloud_Logger.py
@@ -14,11 +14,33 @@ import plotly.express as px
 import librosa as lib
 import librosa.display
 
-st.title("Cloud Logger - Inês Botelho")
+st.title("Cloud Logger - Inês Botelho - Advanced Biomedical Instrumentations Aplications")
 st.markdown(':exclamation: Development of a data logger for information extracted from sound, allowing the storage and visualization of this information collected over a period of time longer than 10 minutes :alarm_clock: :exclamation:')
+st.markdown('''Clicking in the checkbox "Start Micophone Recording :microphone:" it will send a request to the computer running the PC.py file, to record an audio.
+Then this page will receive the information (5s after sending the request) and will show 2 options to show 2 types of graphs (RMS Graph and Sonogram Graph)
+Clicking in the button "Sava Data as CSV File :inbox_tray:" it will save a CSV in your computer with 3 columns (RMS, Time and Sonogram) in wich each line corresponds
+to an adquired audio''')
+
 
 ## Graph with the diagram function of this program
 st_autorefresh(interval=5000)  
+
+st.graphviz_chart('''
+    digraph {
+        subgraph {
+        Microphone -> Computer
+        Computer -> MQTT_Data [color=blue, style=dotted, shape=box]
+        MQTT_Data -> Gitpod [color=blue, style=dotted]
+        MQTT_Request -> Computer [color=blue, style=dotted]
+        Gitpod -> MQTT_Request [color=blue, style=dotted]        
+        MQTT_Request  [shape=box]
+        MQTT_Data  [shape=box]
+        rank = same; MQTT_Request; MQTT_Data;
+        }
+    }
+''')
+st.caption('Diagram representing how this program functions')
+
 
 #MQTT Thread Function
 def MQTT_TH(client):   
@@ -66,6 +88,8 @@ if st.checkbox('Start Voice Recording :microphone:'):
         
         if st.checkbox('Show RMS Graph :sparkles:'):
             st.line_chart(data = df1, x="Times", y="RMS")  
+            st.caption('RMS of the last recorded sound, along the time')
+
         
         # convert it again to list do add to the final dataframe
         rms = df1['RMS'].tolist()
@@ -76,7 +100,6 @@ if st.checkbox('Start Voice Recording :microphone:'):
             # to plot the SFTF
             fig, ax = plt.subplots()
             fig2, ax2 = plt.subplots()
-            st.markdown("### First Chart")
             df2 = list(df2)
             df2 = np.array(df2)
             abso = np.abs(df2)
@@ -86,7 +109,10 @@ if st.checkbox('Start Voice Recording :microphone:'):
             fig.colorbar(img, ax=ax, format="%+2.f dB")
             fig2.colorbar(img2, ax=ax2, format="%+2.f dB")
             st.container().pyplot(fig)
+            st.caption('Intensity of a slot of frequencies of the last recorded sound, along the time')
             st.container().pyplot(fig2)
+            st.caption('Intensity of each pich of the last recorded sound, along the time')
+
 
         # add to the final dataframe the current RMS, Times and STFT
         dataframe_final = st.session_state['dataframe_final']
@@ -103,24 +129,9 @@ else:
 dataframe_final = st.session_state['dataframe_final']
 csv = dataframe_final.to_csv().encode('utf-8')
 st.download_button(
-    label="Download data as CSV",
+    label="Download Data as CSV :inbox_tray:",
     data=csv,
     file_name='data.csv',
     mime='text/csv',
 )
 
-if st.checkbox('Show Diagram :sparkles:'):
-    st.graphviz_chart('''
-        digraph {
-            subgraph {
-            microfone -> computador
-            computador -> mqtt_dados [color=blue, style=dotted, shape=box]
-            mqtt_dados -> gitpod [color=blue, style=dotted]
-            mqtt_pedido -> computador [color=blue, style=dotted]
-            gitpod -> mqtt_pedido [color=blue, style=dotted]        
-            mqtt_pedido  [shape=box]
-            mqtt_dados  [shape=box]
-            rank = same; mqtt_pedido; mqtt_dados;
-            }
-        }
-    ''')
